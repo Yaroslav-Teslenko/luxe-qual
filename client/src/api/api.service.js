65c3cca9aed08993cb3cfc2bc634c906f262e67a
Yaroslav Teslenko <teslenko.yaroslav21@gmail.com>
2021-12-21 14:48:14 +0200

    21-12/21 14:48

--- client/src/api/api.service.js
+++ client/src/api/api.service.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const CL_API_URL = process.env.VUE_APP_CL_API_URL;
 
 const api = axios.create({
-  withCredentials: true,
+  // withCredentials: true,
   baseURL: CL_API_URL,
 });
 const geo = axios.create({
